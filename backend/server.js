46f65e992aec7768ce8fedd5819fc1f508dde9bf
agentVivek <vivek17aug1997@gmail.com>
2025-06-24 19:23:54 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv"; 
+import path from "path";
 
 import authroutes from "./routes/authroutes.js";
 import messageroutes from "./routes/messageroutes.js";
@@ -11,6 +12,7 @@ import {app, server} from "./socket/socket.js" ;
 
 dotenv.config();
 const port = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json()); 
 app.use(cookieParser());
@@ -18,6 +20,11 @@ app.use(cookieParser());
 app.use("/api/auth", authroutes);
 app.use("/api/messages", messageroutes);
 app.use("/api/users", userroutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(port, ()=>{
     console.log(`Server running on port: ${port}`);
